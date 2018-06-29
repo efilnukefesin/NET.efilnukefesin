2bad3bf739b038e1b2ecad88b6d4e78dd79f0fe8
efilnukefesin <efil.threat@googlemail.com>
2018-06-29 23:12:18 +0200

    added some parts and initial documentation

---
+++ NET.efilnukefesin.Contracts.Mvvm/IViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NET.efilnukefesin.Contracts.Mvvm
+{
+    public interface IViewModel : INotifyPropertyChanged
+    {
+    }
+}
