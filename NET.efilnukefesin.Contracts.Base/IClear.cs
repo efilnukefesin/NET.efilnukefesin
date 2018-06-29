a996d625d6810f96af9b96621dd4d049ec2aeee9
efilnukefesin <efil.threat@googlemail.com>
2018-06-29 23:56:53 +0200

    added IClear Interface

---
+++ NET.efilnukefesin.Contracts.Base/IClear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Contracts.Base
+{
+    public interface IClear
+    {
+        #region Methods
+
+        void Clear();
+
+        #endregion Methods
+    }
+}
