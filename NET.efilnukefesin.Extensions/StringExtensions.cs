ed9738dc3b6d57669f3e3a5ee2ba67ec414d18c5
efilnukefesin <efil.threat@googlemail.com>
2018-07-05 22:23:11 +0200

    copied and added StringExtensions class

---
+++ NET.efilnukefesin.Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Extensions
+{
+    public static class StringExtensions
+    {
+        #region ToSha256
+        public static string ToSha256(this string text)
+        {
+            var sha = System.Security.Cryptography.SHA256.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            byte[] hash = sha.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+        #endregion ToSha256
+    }
+}
