48346bd0c84c32a102f68497a969e08de12ec33b
efilnukefesin <efil.threat@googlemail.com>
2018-07-07 16:56:09 +0200

    added result stuff

---
+++ NET.efilnukefesin.Contracts.Base/IResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Contracts.Base
+{
+    public interface IResult<T>
+    {
+        #region Properties
+
+        T Value { get; }
+        bool IsError { get; }
+        IErrorInfo Error { get; }
+
+        #endregion Properties
+    }
+}
---
+++ NET.efilnukefesin.Implementations.Base/Result.cs
@@ -0,0 +1,49 @@
+﻿using NET.efilnukefesin.Contracts.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Implementations.Base
+{
+    public class Result<T> : IResult<T>
+    {
+        #region Properties
+
+        public T Value { get; }
+
+        #region IsError
+        public bool IsError
+        {
+            get
+            {
+                return this.Error == null;
+            }
+        }
+        #endregion IsError
+
+        public IErrorInfo Error { get; }
+
+        #endregion Properties
+
+        #region Construction
+
+        public Result(T Value)
+        {
+            this.Value = Value;
+            this.Error = null;
+        }
+
+        public Result(IErrorInfo Error)
+        {
+            this.Error = Error;
+            this.Value = default(T);
+        }
+
+        #endregion Construction
+
+        #region Methods
+
+        #endregion Methods
+
+    }
+}
