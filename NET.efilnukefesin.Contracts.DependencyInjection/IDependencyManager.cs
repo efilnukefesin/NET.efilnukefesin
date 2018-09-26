e6e7e423b88dcd096d8d2be84e41cf2bd49b213e
efilnukefesin <efil.threat@googlemail.com>
2018-09-26 21:24:40 +0200

    updated contracts

--- NET.efilnukefesin.Contracts.DependencyInjection/IDependencyManager.cs
+++ NET.efilnukefesin.Contracts.DependencyInjection/IDependencyManager.cs
@@ -14,6 +14,7 @@ namespace NET.efilnukefesin.Contracts.DependencyInjection
         T Resolve<T>();
         object Resolve(Type TypeToResolve);
         T Resolve<T>(IDictionary<string, object> parameters);
+        T Resolve<T>(IEnumerable<object> parameters);
         void RegisterType<TFrom, TTo>() where TFrom : class where TTo : class, TFrom;
         void RegisterType<TFrom, TTo>(Lifetime Lifetime) where TFrom : class where TTo : class, TFrom;
         void RegisterInstance<TFrom>(TFrom Instance) where TFrom : class;
