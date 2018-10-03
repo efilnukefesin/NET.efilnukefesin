1019a50367c42c20c9cee0e953d26d7aa43d7d81
efilnukefesin <efil.threat@googlemail.com>
2018-10-03 20:30:53 +0200

    added test classes

---
+++ NET.efilnukefesin.BaseClasses.Test/BaseSimpleTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.BaseClasses.Test
+{
+    /// <summary>
+    /// base class for Unit tests
+    /// </summary>
+    /// <typeparam name="T">the type to be tested</typeparam>
+    [TestClass]
+    public class BaseSimpleTest
+    {
+        #region Properties
+
+        protected Random random;
+
+        #endregion Properties
+
+        #region Methods
+
+        #region Init
+        [TestInitialize]
+        public virtual void Init()
+        {
+            this.random = new Random();
+        }
+        #endregion Init
+
+        #region Deinit
+        [TestCleanup]
+        public virtual void Deinit()
+        {
+        }
+        #endregion Deinit
+
+        #endregion Methods
+    }
+}
---
+++ NET.efilnukefesin.BaseClasses.Test/BaseTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NET.efilnukefesin.BaseClasses.Test
+{
+    /// <summary>
+    /// base class for Unit tests
+    /// </summary>
+    /// <typeparam name="T">the type to be tested</typeparam>
+    [TestClass]
+    public class BaseTest<T>
+    {
+        #region Properties
+
+        protected T target = default(T);
+        protected Random random;
+
+        #endregion Properties
+
+        #region Methods
+
+        #region Init
+        [TestInitialize]
+        public virtual void Init()
+        {
+            this.target = default(T);
+            this.random = new Random();
+        }
+        #endregion Init
+
+        #region Deinit
+        [TestCleanup]
+        public virtual void Deinit()
+        {
+            this.target = default(T);
+        }
+        #endregion Deinit
+
+        #endregion Methods
+    }
+}
