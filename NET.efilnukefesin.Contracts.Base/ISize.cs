0d1ff54218f675917324b26948416bb9941abdb2
efilnukefesin <efil.threat@googlemail.com>
2018-10-28 20:35:21 +0100

    changed the interface and implementation slightly

--- NET.efilnukefesin.Contracts.Base/ISize.cs
+++ NET.efilnukefesin.Contracts.Base/ISize.cs
@@ -6,8 +6,18 @@ namespace NET.efilnukefesin.Contracts.Base
 {
     public interface ISize
     {
-        int Width { get; set; }
-        int Height { get; set; }
+        #region Properties
+
+        float Width { get; set; }
+        float Height { get; set; }
         float AspectRatio { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        ISize Half();
+
+        #endregion Methods
     }
 }
--- NET.efilnukefesin.Implementations.Base/Size.cs
+++ NET.efilnukefesin.Implementations.Base/Size.cs
@@ -9,28 +9,14 @@ namespace NET.efilnukefesin.Implementations.Base
     {
         #region Properties
 
-        #region Width
-        private int width;
-        public int Width
-        {
-            get { return this.width; }
-            set { this.width = value; }
-        }
-        #endregion Width
+        public float Width { get; set; }
 
-        #region Height
-        private int height;
-        public int Height
-        {
-            get { return this.height; }
-            set { this.height = value; }
-        }
-        #endregion Height
+        public float Height { get; set; }
 
         #region AspectRatio
         public float AspectRatio
         {
-            get { return (float)this.width / (float)this.height; }
+            get { return this.Width / this.Height; }
         }
         #endregion AspectRatio
 
@@ -40,19 +26,32 @@ namespace NET.efilnukefesin.Implementations.Base
 
         public Size()
         {
-            this.width = 0;
-            this.height = 0;
+            this.Width = 0f;
+            this.Height = 0f;
         }
 
         public Size(int Width, int Height)
         {
-            this.width = Width;
-            this.height = Height;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public Size(float Width, float Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
         }
 
         #endregion Construction
 
         #region Methods
+
+        #region Half
+        public ISize Half()
+        {
+            return new Size(this.Width / 2f, this.Height / 2f);
+        }
+        #endregion Half
 
         #region ToString
         public override string ToString()
@@ -72,8 +71,8 @@ namespace NET.efilnukefesin.Implementations.Base
             try
             {
                 string[] parts = input.Split('/', '@');
-                result.width = int.Parse(parts[0]);
-                result.height = int.Parse(parts[1]);
+                result.Width = float.Parse(parts[0]);
+                result.Height = float.Parse(parts[1]);
             }
             catch (Exception ex)
             {
