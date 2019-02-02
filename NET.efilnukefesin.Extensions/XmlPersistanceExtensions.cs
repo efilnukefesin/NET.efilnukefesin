7ed8370d6abcd635ab216d19174817b31cea94d4
efilnukefesin <efil.threat@googlemail.com>
2019-02-02 13:31:15 +0100

    added simple serislization methods

---
+++ NET.efilnukefesin.Extensions/XmlPersistanceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace NET.efilnukefesin.Extensions
+{
+    public static class XmlPersistanceExtensions
+    {
+        #region LoadFromXml
+        public static T LoadFromXml<T>(this XElement element)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            MemoryStream msXml = new MemoryStream();
+            element.Save(msXml);
+            msXml.Position = 0;
+            T result = (T)xmlSerializer.Deserialize(msXml);
+            msXml.Close();
+            return result;
+        }
+        #endregion LoadFromXml
+
+        #region SaveToXml
+        public static XElement SaveToXml(this object Input)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(Input.GetType());
+            MemoryStream msXml = new MemoryStream();
+            xmlSerializer.Serialize(msXml, Input);
+            msXml.Position = 0;
+            XElement result = XElement.Load(msXml);
+            msXml.Close();
+            return result;
+        }
+        #endregion SaveToXml
+    }
+}
---
+++ NET.efilnukefesin.Tests.Extensions/XmlPersistanceExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NET.efilnukefesin.BaseClasses.Test;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NET.efilnukefesin.Extensions;
+using System.Xml.Linq;
+using System.IO;
+
+namespace NET.efilnukefesin.Tests.Extensions
+{
+    [TestClass]
+    public class XmlPersistanceExtensionsTests : BaseSimpleTest
+    {
+        #region XmlPersistanceExtensionsMethods
+        [TestClass]
+        public class XmlPersistanceExtensionsMethods : XmlPersistanceExtensionsTests
+        {
+            #region LoadFromXml
+            [TestMethod]
+            public void LoadFromXml()
+            {
+                int i = 0;
+                i = XElement.Parse("<int>16</int>").LoadFromXml<int>();
+            }
+            #endregion LoadFromXml
+
+            #region SaveToXml
+            [TestMethod]
+            public void SaveToXml()
+            {
+                int i = 16;
+
+                var x = i.SaveToXml();
+            }
+            #endregion SaveToXml
+        }
+        #endregion XmlPersistanceExtensionsMethods
+    }
+}
