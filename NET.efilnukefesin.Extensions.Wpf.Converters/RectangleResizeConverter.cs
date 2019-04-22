343ec3bd4e216dc508dc32d354d98728cd765e57
efilnukefesin <efil.threat@googlemail.com>
2019-04-22 21:57:39 +0200

    set the stage for the RectangleResizeConverter.

---
+++ NET.efilnukefesin.Extensions.Wpf.Converters/RectangleResizeConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NET.efilnukefesin.Extensions.Wpf.Converters
+{
+    /// <summary>
+    /// Converter for Rectangle Resizing, useable for generating a bigger or smaller rectangle than the source.
+    /// </summary>
+    public class RectangleResizeConverter : IValueConverter
+    {
+        #region Methods
+
+        #region Convert
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion Convert
+
+        #region ConvertBack
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion ConvertBack
+
+        #endregion Methods
+    }
+}
---
+++ NET.efilnukefesin.Tests.Extensions.Wpf.Converters/RectangleResizeConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NET.efilnukefesin.BaseClasses.Test;
+using NET.efilnukefesin.Extensions.Wpf.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Tests.Extensions.Wpf.Converters
+{
+    [TestClass]
+    public class RectangleResizeConverterTests : BaseSimpleTest
+    {
+        #region RectangleResizeConverterProperties
+        [TestClass]
+        public class RectangleResizeConverterProperties : RectangleResizeConverterTests
+        {
+
+        }
+        #endregion RectangleResizeConverterProperties
+
+        #region RectangleResizeConverterConstruction
+        [TestClass]
+        public class RectangleResizeConverterConstruction : RectangleResizeConverterTests
+        {
+
+        }
+        #endregion RectangleResizeConverterConstruction
+
+        #region RectangleResizeConverterMethods
+        [TestClass]
+        public class RectangleResizeConverterMethods : RectangleResizeConverterTests
+        {
+            #region Convert
+            [TestMethod]
+            public void Convert()
+            {
+                RectangleResizeConverter rectangleResizeConverter = new RectangleResizeConverter();
+                throw new NotImplementedException();
+            }
+            #endregion Convert
+
+            #region ConvertBack
+            [TestMethod]
+            public void ConvertBack()
+            {
+                RectangleResizeConverter rectangleResizeConverter = new RectangleResizeConverter();
+                throw new NotImplementedException();
+            }
+            #endregion ConvertBack
+        }
+        #endregion RectangleResizeConverterMethods
+    }
+
+}
