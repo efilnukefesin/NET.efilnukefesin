dc3ef4d09f7d4b35041b81c764efbb942db7a4e4
efilnukefesin <efil.threat@googlemail.com>
2019-05-15 21:55:53 +0200

    added base test.

---
+++ test/NET.efilnukefesin.Tests.Implementations.DependencyInjection/DiManagerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NET.efilnukefesin.BaseClasses.Test;
+using NET.efilnukefesin.Contracts.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Tests.Implementations.DiManager
+{
+    [TestClass]
+    public class DiManagerTests : BaseSimpleTest
+    {
+        #region DiManagerProperties
+        [TestClass]
+        public class DiManagerProperties : DiManagerTests
+        {
+
+        }
+        #endregion DiManagerProperties
+
+        #region DiManagerConstruction
+        [TestClass]
+        public class DiManagerConstruction : DiManagerTests
+        {
+            #region Construct
+            [TestMethod]
+            public void Construct()
+            {
+                IDependencyManager result = NET.efilnukefesin.Implementations.DependencyInjection.DiManager.GetInstance();
+
+                Assert.IsNotNull(result);
+            }
+            #endregion Construct
+        }
+        #endregion DiManagerConstruction
+
+        #region DiManagerMethods
+        [TestClass]
+        public class DiManagerMethods : DiManagerTests
+        {
+            
+        }
+        #endregion DiManagerMethods
+    }
+}
