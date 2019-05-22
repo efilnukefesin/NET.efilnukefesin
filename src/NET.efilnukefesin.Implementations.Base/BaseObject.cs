570571d3e6f99a252afbb77e0db2fb71eb0d89a0
efilnukefesin <efil.threat@googlemail.com>
2019-05-22 18:52:09 +0200

    tried to make more compatible with net core 3.0 preview 5

--- src/NET.efilnukefesin.Implementations.Base/BaseObject.cs
+++ src/NET.efilnukefesin.Implementations.Base/BaseObject.cs
@@ -1,4 +1,5 @@
 ﻿using NET.efilnukefesin.Contracts.Base;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
@@ -8,6 +9,7 @@ using System.Xml.Serialization;
 namespace NET.efilnukefesin.Implementations.Base
 {
     [DataContract]
+    [JsonObject]
     public abstract class BaseObject : BaseDisposable, IBaseObject
     {
         #region Properties
