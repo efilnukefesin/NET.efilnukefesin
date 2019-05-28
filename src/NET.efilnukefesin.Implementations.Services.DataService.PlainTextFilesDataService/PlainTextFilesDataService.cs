4fd0e028fd8f070a5fc65b33f9af02ab9d79256e
efilnukefesin <efil.threat@googlemail.com>
2019-05-28 10:43:11 +0200

    prepared new data service implementation

---
+++ src/NET.efilnukefesin.Implementations.Services.DataService.PlainTextFilesDataService/PlainTextFilesDataService.cs
@@ -0,0 +1,11 @@
+﻿using NET.efilnukefesin.Contracts.Services.DataService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Implementations.Services.DataService.PlainTextFilesDataService
+{
+    public class PlainTextFilesDataService : IDataService
+    {
+    }
+}
