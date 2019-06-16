d33efc5ed7c44b207b2368ce7c7ff207f649d163
efilnukefesin <efil.threat@googlemail.com>
2019-06-16 21:33:14 +0200

    added IInitalize interface

---
+++ src/NET.efilnukefesin.Contracts.Base/IInitalize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.efilnukefesin.Contracts.Base
+{
+    public interface IInitalize
+    {
+        #region Properties
+
+        bool IsInitialized { get; set; }
+
+        #endregion Properties
+
+        #region Methods
+        Task<bool> Initialize();
+        #endregion Methods
+    }
+}
