91f1dd63fa923fd1f27e8cb9c675bcc3432647dc
efilnukefesin <efil.threat@googlemail.com>
2019-06-20 20:43:14 +0200

    added comments and logging

--- src/NET.efilnukefesin.Contracts.FeatureToggling/IFeatureToggleManager.cs
+++ src/NET.efilnukefesin.Contracts.FeatureToggling/IFeatureToggleManager.cs
@@ -8,10 +8,39 @@ namespace NET.efilnukefesin.Contracts.FeatureToggling
     {
         #region Methods
 
-        void Add(IFeatureToggle FeatureToggle);
+        #region Add
+        /// <summary>
+        /// adds a new featuretoggle
+        /// </summary>
+        /// <param name="FeatureToggle">the featuretoggle to add</param>
+        /// <returns>wether adding was successful or not</returns>
+        bool Add(IFeatureToggle FeatureToggle);
+        #endregion Add
+
+        #region GetValue: returns wether the toggle is on or off
+        /// <summary>
+        /// returns wether the toggle is on or off
+        /// </summary>
+        /// <param name="Name">the name of the toggle</param>
+        /// <returns>the value of the toggle or false if not found</returns>
         bool GetValue(string Name);
+        #endregion GetValue
+
+        #region Exists: indicates, if a Feature Toggle with the specified name already exists
+        /// <summary>
+        /// indicates, if a Feature Toggle with the specified name already exists
+        /// </summary>
+        /// <param name="Name">the name of the Toggle</param>
+        /// <returns>true if it is already known, false if not</returns>
         bool Exists(string Name);
+        #endregion Exists
+
+        #region Clear: resets all known toggles to zero
+        /// <summary>
+        /// resets all known toggles to zero
+        /// </summary>
         void Clear();
+        #endregion Clear
 
         #endregion Methods
     }
--- src/NET.efilnukefesin.Implementations.FeatureToggling/FeatureToggleManager.cs
+++ src/NET.efilnukefesin.Implementations.FeatureToggling/FeatureToggleManager.cs
@@ -29,38 +29,78 @@ namespace NET.efilnukefesin.Implementations.FeatureToggling
 
         #region Methods
 
-        #region Add
-        public void Add(IFeatureToggle FeatureToggle)
+        #region Add: adds a new featuretoggle
+        /// <summary>
+        /// adds a new featuretoggle
+        /// </summary>
+        /// <param name="FeatureToggle">the featuretoggle to add</param>
+        /// <returns>wether adding was successful or not</returns>
+        public bool Add(IFeatureToggle FeatureToggle)
         {
-            this.toggles.Add(FeatureToggle);
+            bool result = false;
+            this.logger?.Log($"FeatureToggleManager.Add: started with '{FeatureToggle.Name}'");
+            if (this.Exists(FeatureToggle.Name))
+            {
+                this.logger?.Log($"FeatureToggleManager.Add: '{FeatureToggle.Name}' already exists, not added!", Contracts.Logger.Enums.LogLevel.Warning);
+            }
+            else
+            {
+                this.toggles.Add(FeatureToggle);
+                this.logger?.Log($"FeatureToggleManager.Add: added '{FeatureToggle.Name}'");
+                result = true;
+            }
+            
+            this.logger?.Log($"FeatureToggleManager.Add: ended with '{FeatureToggle.Name}', result is '{result}'");
+            return result;
         }
         #endregion Add
 
-        #region GetValue
+        #region GetValue: returns wether the toggle is on or off
+        /// <summary>
+        /// returns wether the toggle is on or off
+        /// </summary>
+        /// <param name="Name">the name of the toggle</param>
+        /// <returns>the value of the toggle or false if not found</returns>
         public bool GetValue(string Name)
         {
             bool result = false;
+            this.logger?.Log($"FeatureToggleManager.GetValue: started with '{Name}'");
 
             if (this.Exists(Name))
             {
                 result = this.toggles.Where(x => x.Name.Equals(Name)).FirstOrDefault().GetIsActive();
             }
 
+            this.logger?.Log($"FeatureToggleManager.GetValue: ended with '{Name}', result is '{result}'");
             return result;
         }
         #endregion GetValue
 
-        #region Exists
+        #region Exists: indicates, if a Feature Toggle with the specified name already exists
+        /// <summary>
+        /// indicates, if a Feature Toggle with the specified name already exists
+        /// </summary>
+        /// <param name="Name">the name of the Toggle</param>
+        /// <returns>true if it is already known, false if not</returns>
         public bool Exists(string Name)
         {
-            return this.toggles.Any(x => x.Name.Equals(Name));
+            bool result = false;
+            this.logger?.Log($"FeatureToggleManager.Exists: started with '{Name}'");
+            result = this.toggles.Any(x => x.Name.Equals(Name));
+            this.logger?.Log($"FeatureToggleManager.Exists: ended with '{Name}', result is '{result}'");
+            return result;
         }
         #endregion Exists
 
-        #region Clear
+        #region Clear: resets all known toggles to zero
+        /// <summary>
+        /// resets all known toggles to zero
+        /// </summary>
         public void Clear()
         {
+            this.logger?.Log($"FeatureToggleManager.Clear: started");
             this.toggles.Clear();
+            this.logger?.Log($"FeatureToggleManager.Clear: ended");
         }
         #endregion Clear
 
