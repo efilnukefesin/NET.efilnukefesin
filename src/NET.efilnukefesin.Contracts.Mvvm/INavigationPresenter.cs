d73868d562d4ac75bece6521159323afd261dc1d
efilnukefesin <efil.threat@googlemail.com>
2019-07-05 20:48:32 +0200

    Added Property to INavigationPresenter: IsPresenterRegistered

--- src/NET.efilnukefesin.Contracts.Mvvm/INavigationPresenter.cs
+++ src/NET.efilnukefesin.Contracts.Mvvm/INavigationPresenter.cs
@@ -7,6 +7,17 @@ namespace NET.efilnukefesin.Contracts.Mvvm
 {
     public interface INavigationPresenter : IBaseObject
     {
+        #region Properties
+
+        #region IsPresenterRegistered: indicates, if the presenter is ready to present
+        /// <summary>
+        /// indicates, if the presenter is ready to present
+        /// </summary>
+        bool IsPresenterRegistered { get; }
+        #endregion IsPresenterRegistered
+
+        #endregion Properties
+
         #region Methods
         void RegisterPresenter(object Presenter);
         bool Present(string ViewUri, object DataContext);
