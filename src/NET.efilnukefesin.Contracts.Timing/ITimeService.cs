8f663408c63414e176c4b43d2b7839c1ebb845c7
efilnukefesin <efil.threat@googlemail.com>
2019-08-14 11:27:33 +0200

    defined interface.

--- src/NET.efilnukefesin.Contracts.Timing/ITimeService.cs
+++ src/NET.efilnukefesin.Contracts.Timing/ITimeService.cs
@@ -27,6 +27,62 @@ namespace NET.efilnukefesin.Contracts.Timing
 
         #region Methods
 
+        #region Play: puts the progress of ElapsedTimeRelative back to standard
+        /// <summary>
+        /// puts the progress of ElapsedTimeRelative back to standard
+        /// </summary>
+        void Play();
+        #endregion Play
+
+        #region FastForward: Fast forwards ElapsedTimeRelative
+        /// <summary>
+        /// Fast forwards ElapsedTimeRelative
+        /// </summary>
+        /// <param name="Multiplier">The Multiplier to be applied, e.g. 0.5 for half speed or slo-mo</param>
+        void FastForward(double Multiplier);
+        #endregion FastForward
+
+        #region FastForward: Fast forwards ElapsedTimeRelative to a certain point in time since start
+        /// <summary>
+        /// Fast forwards ElapsedTimeRelative to a certain point in time since start
+        /// </summary>
+        /// <param name="Multiplier">The Multiplier to be applied, e.g. 0.5 for half speed or slo-mo</param>
+        /// <param name="TargetPoint">the target time in terms of Elapsed Time since start</param>
+        void FastForward(double Multiplier, TimeSpan TargetPoint);
+        #endregion FastForward
+
+        #region Rewind: Rewinds ElapsedTimeRelative
+        /// <summary>
+        /// Rewinds ElapsedTimeRelative
+        /// </summary>
+        /// <param name="Multiplier">The Multiplier to be applied, e.g. 0.5 for half speed or slo-mo</param>
+        void Rewind(double Multiplier);
+        #endregion Rewind
+
+        #region Rewind: Rewinds ElapsedTimeRelative to a certain point in time since start
+        /// <summary>
+        /// Rewinds ElapsedTimeRelative to a certain point in time since start
+        /// </summary>
+        /// <param name="Multiplier">The Multiplier to be applied, e.g. 0.5 for half speed or slo-mo</param>
+        /// <param name="TargetPoint">the target time in terms of Elapsed Time since start</param>
+        void Rewind(double Multiplier, TimeSpan TargetPoint);
+        #endregion Rewind
+
+        #region Pause: puts the progress of ElapsedTimeRelative on 'paused', so no progress at all
+        /// <summary>
+        /// puts the progress of ElapsedTimeRelative on 'paused', so no progress at all
+        /// </summary>
+        void Pause();
+        #endregion Pause
+
+        #region JumpTo: Jumps directly to a target time for ElapsedTimeRelative
+        /// <summary>
+        /// Jumps directly to a target time for ElapsedTimeRelative 
+        /// </summary>
+        /// <param name="TargetPoint">the target time in terms of Elapsed Time since start</param>
+        void JumpTo(TimeSpan TargetPoint);
+        #endregion JumpTo
+
         #endregion Methods
     }
 }
